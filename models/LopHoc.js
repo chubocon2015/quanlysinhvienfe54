d59bf103c0bfc955aa6331e84ec3e09e3e3b08c2
unknown <chubocon2015@gmail.com>
2020-11-08 15:47:14 +0700

    update2

--- models/LopHoc.js
+++ models/LopHoc.js
@@ -1,5 +1,6 @@
 var LopHoc = function(){
     this.danhsachSinhVien = [];
+    this.moTa = 'Day lop FE 54 đang học git'
     this.layDanhSachSinhVien = function(){
         return this.danhsachSinhVien;
     }
